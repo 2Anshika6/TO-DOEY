97d26817a741166bc5b94c5546e321c316169a12
2Anshika6 <prasad.anshika2018@gmail.com>
2023-10-01 01:27:30 +0530

    to-do-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { TodoWrapper } from "./components/TodoWrapper";
+import LoginSignup from './components/loginsignup/loginsignup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+        <Route path='/todolist' element={<TodoWrapper />} />
+        <Route path='/' element={<LoginSignup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
